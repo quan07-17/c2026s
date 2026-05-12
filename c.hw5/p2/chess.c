cea8c9c6a1c70143c99fc6e3f113bb84b20ba624
Quan <a0901332166@gmail.com>
2026-05-13 02:12:35 +0800

    chess.c add horizontal cheack

--- c.hw5/p2/chess.c
+++ c.hw5/p2/chess.c
@@ -1,1 +1,37 @@
-void isLiveThree(char (*p)[19]);
+#include "chess.h"
+
+bool checkHorizontal(char (*p)[5])
+{
+    int i, b = 0, w = 0;
+    for (i = 1; i < 4; i++)
+    {
+        if ((*p)[i] == '1') {
+            b++;
+        }
+        else if ((*p)[i] == '0'){
+            w++;
+        }
+    }
+    if ((b == 3 || w == 3) && ((*p)[0] == '.' && (*p)[4] == '.')) {
+        return true;
+    }
+    return false;
+}
+void isLiveThree(char chessboard[19][19])
+{
+    int i, j;
+    bool liveThree = false;
+    for (i = 0; i < 19; i++) {
+        for (j = 0; j < 19; j++) {
+            if (j <= 14) {
+                liveThree = checkHorizontal((char (*)[5])&chessboard[i][j]);
+                if (liveThree) {
+                    printf("LiveThree found: (%c, %2d)(%c, %2d)(%c, %2d)\n", j + 'A' + 1, i + 1, j + 2 + 'A', i + 1, j + 3 + 'A', i + 1);
+                    return;
+                }
+            }
+        }
+    }
+    printf("Live three does not exist.\n");
+    return;
+}
--- c.hw5/p2/chess.h
+++ c.hw5/p2/chess.h
@@ -1,1 +1,9 @@
-void isLiveThree(char (*p)[19]);
+#include <stdio.h>
+#define bool char
+#define true 1
+#define false 0
+
+typedef enum {Black=49, White=48, None=0} Player;
+
+bool checkHorizontal(char (*p)[5]);
+void isLiveThree(char chessboard[19][19]);
